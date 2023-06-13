1dfbb41257fe0ac4a7d54cd6d8cfdcf0986bb2d8
Sayed Shah <133984128+sshahmuneer@users.noreply.github.com>
2023-06-13 18:33:05 -0400

    Create function playRound for one round of rock paper scissors

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === 'rock') {
+        if (computerSelection === 'rock') {
+            return 'Tie!';
+        } else if (computerSelection === 'paper') {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else if (computerSelection === 'scissors') {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    } else if (playerSelection.toLowerCase() === 'paper') {
+        if (computerSelection === 'rock') {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection === 'paper') {
+            return 'Tie!';
+        } else if (computerSelection === 'scissors') {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    } else if (playerSelection.toLowerCase() === 'scissors') {
+        if (computerSelection === 'rock') {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else if (computerSelection === 'paper') {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection === 'scissors') {
+            return 'Tie!';
+        }
+    }
+}
